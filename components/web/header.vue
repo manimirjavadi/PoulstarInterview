99c44a5f8b5e88c8910012307a52dbed3a1b3819
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2023-05-02 12:23:49 +0330

    [fix] MoreMenu: Mouse hover done

--- components/web/header.vue
+++ components/web/header.vue
@@ -1,14 +1,25 @@
+<script setup>
+const moreMenu = ref(false);
+</script>
+
 <template>
   <div class="bg-white shadow-default py-[25px]">
     <div class="container flex items-center justify-between">
       <div class="flex items-center">
         <nuxt-link to="#" class="ml-[50px]">
-          <img src="@/assets/icons/web/header/logo.png" alt="logo">
+          <img src="@/assets/icons/web/header/logo.png" alt="logo" />
         </nuxt-link>
         <div class="flex items-center gap-5">
-          <nuxt-link to="#" class="text-gray text-lg font-semibold">صفحه اصلی</nuxt-link>
+          <nuxt-link to="#" class="text-gray text-lg font-semibold"
+            >صفحه اصلی</nuxt-link
+          >
           <div class="relative" ref="courseRef">
-            <div @click="courseMenu = !courseMenu" class="text-gray text-lg font-semibold cursor-pointer">دوره های اصلی</div>
+            <div
+              @click="courseMenu = !courseMenu"
+              class="text-gray text-lg font-semibold cursor-pointer"
+            >
+              دوره های اصلی
+            </div>
             <transition
               enter-active-class="transition duration-100 ease-out"
               enter-from-class="transform scale-95 opacity-0"
@@ -17,32 +28,73 @@
               leave-from-class="transform scale-100 opacity-100"
               leave-to-class="transform scale-95 opacity-0"
             >
-              <div class="flex gap-5 p-[25px] absolute right-0 mt-2 min-w-max w-full rounded-[10px] z-20 bg-white shadow-default" v-if="courseMenu">
+              <div
+                class="flex gap-5 p-[25px] absolute right-0 mt-2 min-w-max w-full rounded-[10px] z-20 bg-white shadow-default"
+                v-if="courseMenu"
+              >
                 <div class="flex flex-col gap-5">
-                  <nuxt-link to="#" class="flex items-center gap-[10px]" @click="courseMenu = false">
-                    <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
+                  <nuxt-link
+                    to="#"
+                    class="flex items-center gap-[10px]"
+                    @click="courseMenu = false"
+                  >
+                    <span
+                      class="flex w-2 h-2 rounded-full bg-gray_light"
+                    ></span>
                     <div class="text-gray_medium text-lg">همه دوره ها</div>
                   </nuxt-link>
-                  <nuxt-link to="#" class="flex items-center gap-[10px]" @click="courseMenu = false">
-                    <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
-                    <div class="text-gray_medium text-lg">پیش شتابدهی استعداد</div>
+                  <nuxt-link
+                    to="#"
+                    class="flex items-center gap-[10px]"
+                    @click="courseMenu = false"
+                  >
+                    <span
+                      class="flex w-2 h-2 rounded-full bg-gray_light"
+                    ></span>
+                    <div class="text-gray_medium text-lg">
+                      پیش شتابدهی استعداد
+                    </div>
                   </nuxt-link>
-                  <nuxt-link to="#" class="flex items-center gap-[10px]" @click="courseMenu = false">
-                    <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
+                  <nuxt-link
+                    to="#"
+                    class="flex items-center gap-[10px]"
+                    @click="courseMenu = false"
+                  >
+                    <span
+                      class="flex w-2 h-2 rounded-full bg-gray_light"
+                    ></span>
                     <div class="text-gray_medium text-lg">شتابدهی استعداد</div>
                   </nuxt-link>
-                  <nuxt-link to="#" class="flex items-center gap-[10px]" @click="courseMenu = false">
-                    <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
+                  <nuxt-link
+                    to="#"
+                    class="flex items-center gap-[10px]"
+                    @click="courseMenu = false"
+                  >
+                    <span
+                      class="flex w-2 h-2 rounded-full bg-gray_light"
+                    ></span>
                     <div class="text-gray_medium text-lg">نیترو</div>
                   </nuxt-link>
                 </div>
-                <img src="@/assets/images/web/public/courses.jpg" alt="courses" class="rounded-default">
+                <img
+                  src="@/assets/images/web/public/courses.jpg"
+                  alt="courses"
+                  class="rounded-default"
+                />
               </div>
             </transition>
           </div>
-          <nuxt-link to="#" class="text-gray text-lg font-semibold">دوره های مکمل</nuxt-link>
+          <nuxt-link to="#" class="text-gray text-lg font-semibold"
+            >دوره های مکمل</nuxt-link
+          >
           <div class="relative" ref="moreRef">
-            <nuxt-icon name="web/header/more" filled @click="moreMenu = !moreMenu" class="cursor-pointer"/>
+            <nuxt-icon
+              name="web/header/more"
+              filled
+              @mouseover="moreMenu = true"
+              @mouseleave="moreMenu = false"
+              class="cursor-pointer"
+            />
             <transition
               enter-active-class="transition duration-100 ease-out"
               enter-from-class="transform scale-95 opacity-0"
@@ -51,16 +103,27 @@
               leave-from-class="transform scale-100 opacity-100"
               leave-to-class="transform scale-95 opacity-0"
             >
-              <div class="grid grid-cols-3 gap-5 p-[25px] absolute right-0 mt-2 min-w-max w-full rounded-[10px] z-20 bg-white shadow-default" v-if="moreMenu">
+              <div
+                class="grid grid-cols-3 gap-5 p-[25px] absolute right-0 mt-2 min-w-max w-full rounded-[10px] z-20 bg-white shadow-default"
+                v-if="moreMenu"
+              >
                 <!-- <nuxt-link to="#calendar" class="flex items-center gap-[10px]" @click="moreMenu = false">
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">تقویم آموزشی</div>
                 </nuxt-link> -->
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">پادکست</div>
                 </nuxt-link>
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">وبلاگ</div>
                 </nuxt-link>
@@ -69,29 +132,53 @@
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">تعیین سطح</div>
                 </nuxt-link> -->
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">ویدیوها</div>
                 </nuxt-link>
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">دعوت به همکاری</div>
                 </nuxt-link>
 
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">ارتباط با ما</div>
                 </nuxt-link>
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">درباره ما</div>
                 </nuxt-link>
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">درخواست نمایندگی</div>
                 </nuxt-link>
- 
-                <nuxt-link to="#" class="flex items-center gap-[10px]" @click="moreMenu = false">
+
+                <nuxt-link
+                  to="#"
+                  class="flex items-center gap-[10px]"
+                  @click="moreMenu = false"
+                >
                   <span class="flex w-2 h-2 rounded-full bg-gray_light"></span>
                   <div class="text-gray_medium text-lg">اسپانسرینگ</div>
                 </nuxt-link>
@@ -101,7 +188,11 @@
         </div>
       </div>
       <div class="flex items-center">
-        <nuxt-icon name="web/header/search" filled class="rounded-full p-[10px] hover:bg-[#e2e3e5] cursor-pointer ease-in-out duration-150"/>
+        <nuxt-icon
+          name="web/header/search"
+          filled
+          class="rounded-full p-[10px] hover:bg-[#e2e3e5] cursor-pointer ease-in-out duration-150"
+        />
         <nuxt-link to="#" class="mx-5 border-2 border-yellow btn">
           <nuxt-icon name="web/header/user-square" filled />
           <span class="text-yellow font-semibold text-lg">ثبت نام دوره ها</span>
@@ -165,6 +256,6 @@
 }
 .otp-input.is-complete.error {
   border-color: #ed1c25;
-  color: #ed1c25
+  color: #ed1c25;
 }
 </style>
